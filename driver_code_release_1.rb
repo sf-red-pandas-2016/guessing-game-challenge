435714aeade5444152981234236afd26bf5bb609
Jim Lengel & Sean Lee <pair+jimlengel.seanyboy49@devbootcamp.com>
2016-07-21 15:00:36 -0700

    complete preliminary release 1

--- driver_code_release_1.rb
+++ driver_code_release_1.rb
@@ -23,6 +23,9 @@ p (game.has_lost? == false)
 puts "Wrong guess receives correct feedback"
 p (game.guess(2) == "Too low!")
 
+p game.guess(2)
+p " = = = = = = = = ="
+
 puts "Wrong guess deducts from remaining guesses"
 p (game.remaining_guesses == 4)
 
@@ -55,6 +58,7 @@ p (winning_game.guess(100) == "Yay, you won! The number was 100")
 puts "Losing games get a loss message"
 p (losing_game.guess(9) == "You lost! The number was 999")
 p (copied_game.guess(123) == "You lost! The number was 8")
+p losing_game.guess(9)
 
 puts "Guesses on completed games show the status of the game"
 p (game.guess(8) == "You already won. The number was 8")
@@ -62,7 +66,6 @@ p (game.guess(9) == "You already won. The number was 8")
 p (winning_game.guess(0) == "You already won. The number was 100")
 p (losing_game.guess(999) == "You already lost. The number was 999")
 p (losing_game.guess(111) == "You already lost. The number was 999")
-
 
 puts "Guesses on completed games do not alter remaining_guesses"
 p (game.remaining_guesses == 0)
--- guessing_game.rb
+++ guessing_game.rb
@@ -1,13 +1,14 @@
 
 class GuessingGame
-  attr_accessor :congrats_message, :game_status
+  attr_accessor :congrats_message, :game_won, :game_lost
 
   def initialize(secret_number, allowed_guesses)
     @secret_number = secret_number
     @allowed_guesses = allowed_guesses
     @congrats_message = "Yay, you won!"
     @guesses = []
-    @game_status = false
+    @game_won = false
+    @game_lost = false
 
   end
 
@@ -16,7 +17,7 @@ class GuessingGame
   end
 
   def has_won?
-    @game_status
+    @game_won
   end
 
   def has_lost?
@@ -24,44 +25,62 @@ class GuessingGame
   end
 
   def guess(player_guess)
-    if has_won?
-      "You already won. The number was #{@secret_number}"
-    elsif remaining_guesses == 0
-      "You already lost. The number was #{@secret_number}"
-      @game_lost = true
-    else
-
-      @guesses << player_guess
-
-      if player_guess == @secret_number
-        @game_status = true
-        @congrats_message + " The number was #{@secret_number}"
+    @guesses << player_guess
+    message = ""
+    if player_guess == @secret_number
+      if @game_won
+        message = "You already won. The number was #{@secret_number}"
       else
-        if has_lost?
-          "You lost! The number was #{@secret_number}"
+        message = @congrats_message + " The number was #{@secret_number}"
+        @game_won = true
+      end
+    else
+      if @game_lost
+        message = "You already lost. The number was #{@secret_number}"
+      else
+        if player_guess < @secret_number
+          message = "Too low!"
         else
-          if remaining_guesses == 1
-            "Too high! WARNING: Only one guess left!"
-          else
-            player_guess < @secret_number ? "Too low!" : "Too high!"
-          end
+          message = "Too high!"
         end
+
+        if remaining_guesses == 1
+          message += " WARNING: Only one guess left!"
+        elsif remaining_guesses == 0
+          message += "You lost! The number was #{@secret_number}"
+        else
+        " asd ads as "
+        end
+
       end
     end
-
-
-    # add the guess to the array
-    # if player_guess == @secret_number
-    #   use has_won to check if the user has won
-    #   return congrats message
-
-    # elsif player_guess != secret_number
-    #   too high
-    #   too low
-
-
-
-
   end
+
+
+      # # # # # # # # # # # # # # # # # # # # # # # # #
+
+    # if has_won?
+    #   "You already won. The number was #{@secret_number}"
+    #   # @game_won = true
+    # elsif has_lost? #remaining_guesses == 0
+    #   "You already lost. The number was #{@secret_number}"
+    # else
+    #   @guesses << player_guess
+    #   if player_guess == @secret_number
+    #     @game_won = true
+    #     @congrats_message + " The number was #{@secret_number}"
+    #   else
+    #     if remaining_guesses == 0
+    #       "You lost! The number was #{@secret_number}"
+    #       @game_lost = true
+    #     else
+    #       if remaining_guesses == 1
+    #         "Too high! WARNING: Only one guess left!"
+    #       else
+    #         player_guess < @secret_number ? "Too low!" : "Too high!"
+    #       end
+    #     end
+    #   end
+    # end
 
 end
