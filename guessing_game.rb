37ed89b29cf2145cf82ebb0140b5349f7e0b8c75
Armon Arcuri & Kevin McGovern <pair+armoney.kevinmcgovern92@devbootcamp.com>
2016-07-21 12:07:04 -0700

    Everything evaluates as true except for line 73 of driver code

---
+++ guessing_game.rb
@@ -0,0 +1,67 @@
+class GuessingGame
+
+  attr_reader :num_guesses
+  attr_accessor :congrats_message
+
+  def initialize(secret_num, num_guesses)
+    @secret_num = secret_num
+    @num_guesses = num_guesses
+    @congrats_message = "Yay, you won!"
+    @guesses_made = []
+    @won = false
+    @lost = true
+  end
+
+  def remaining_guesses
+    return @num_guesses
+  end
+
+  def has_won?
+    @won
+  end
+
+  def has_lost?
+    @num_guesses <= 0 if !has_won?
+  end
+
+  def guess(guessed_num)
+    return "You already won. The number was #{@secret_num}" if has_won?
+    return "You already lost. The number was #{@secret_num}" if has_lost?
+    if guessed_num == @secret_num
+       @won = true
+       @num_guesses -= 1
+      return @congrats_message + " The number was #{@secret_num}"
+    end
+    unless guessed_num == @secret_num
+      response = ""
+      if @guesses_made.include?(guessed_num)
+        if guessed_num < @secret_num
+          response += "Too low!"
+          # @num_guesses -= 1
+        else
+          response += "Too high!"
+          # @num_guesses -= 1
+        end
+      else
+        if guessed_num < @secret_num
+          response += "Too low!"
+          @num_guesses -= 1 if @num_guesses > 0
+        else
+          response += "Too high!"
+          @num_guesses -= 1 if @num_guesses > 0
+        end
+      end
+      @guesses_made << guessed_num
+      if @num_guesses > 1
+        return response
+      elsif @num_guesses == 1
+        response += " WARNING: Only one guess left!"
+        return response
+      else
+        #@lost = false
+        "You lost! The number was #{@secret_num}"
+      end
+    end
+  end
+
+end
